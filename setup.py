3afa4553d485dbc6c3abc5af00fea0a86e0ea7be
Pitchfork :3 <kanonenwagen7@gmail.com>
2025-11-21 16:24:11 -0500

    Enhance setup.py with long description
    Added long description and content type from README.md

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='moduleName',
@@ -10,6 +13,8 @@ setup(
     license='MIT',
     packages=['moduleName'],
     install_requires=[],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     classifiers=[
         'Development Status :: 4 - Beta',
